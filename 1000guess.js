809b5465a34acad35951fa775a44102be8268bea
盧瑞山 <luruishan@luruishande-MacBook-Pro.local>
2018-01-03 03:49:49 +0800

    fixed

--- 1000guess.js
+++ 1000guess.js
@@ -6,24 +6,9 @@ console.log('contractInstance',contractInstance);
 /**
  * 宣告事件變數 監聽從0塊到此合約當下的 SentPrizeToWinne SentDeveloperFee
  */
-let eventSentPrizeToWinner=contractInstance.SentPrizeToWinner(null,{fromBlock:0,toBlock:'latest'});
+// let eventSentPrizeToWinner=contractInstance.SentPrizeToWinner(null,{fromBlock:0,toBlock:'latest'}); //用不到了,改以getTransactionReceipt抓取
 let eventSentDeveloperFee=contractInstance.SentDeveloperFee(null,{fromBlock:0,toBlock:'latest'});
 
-
-/**
- * 監聽事件發生 並寫入表格中
- */
-eventSentPrizeToWinner.watch((e,r)=>{
-    let {args: result} = JSON.parse(JSON.stringify(r)); // ES6 賦值語法 宣告變數名稱 result 來自args ex: let {args: result} = {args:value}
-    let row=`<tr><td>${result.gameindex}</td><td>${result.guess}</td><td>${result.lotterynumber}</td><td>${result.money}</td><td>${result.timestamp}</td><td>${result.winner}</td></tr>`
-    $('#eventSentPrizeToWinnerResult').append(row)
-})
-let index=0;
-eventSentDeveloperFee.watch((e,r)=>{
-    let {args: result} = JSON.parse(JSON.stringify(r)); 
-    let row=`<tr><td>${++index}</td><td>${result.amount}</td><td>${result.balance}</td></tr>`
-    $('#eventSentDeveloperFeeResult').append(row)
-})
 
 /**
  * 定時抓取特定某些狀態
@@ -56,6 +41,7 @@ function call(contractFunction,...args){
             console.log('call',e,result);
             if(e) observer.error(e);    // 如果有 e (error) 則拋出 observer.error 例外
             else observer.next(result); // 否則將 result 用 observer.next 送出使 Observable 觀察
+            observer.complete();
         })
 	})
 }
@@ -71,6 +57,7 @@ function send(contractFunction,...args){
             console.log('send',e,txid);
             if(e) observer.error(e);
             else observer.next(txid);
+            observer.complete();
         })
 	})
 }
@@ -80,16 +67,34 @@ function send(contractFunction,...args){
  * @param {string} txid 
  */
 function getReceipt(txid){
+    console.log(txid)
     return Rx.Observable.create((observer) => {
 	    web3.eth.getTransactionReceipt(txid,(e,result)=>{
-            console.log('receipt',e,result);
+            // console.log('receipt',e,result);
             if(e) observer.error(e);
             else observer.next(result);
+            observer.complete();
         })
 	})
 }
 
+function decodeSentPrizeToWinner(data) {
+    let winner = "0x" + data.substr(2+24,40)
+    let money = parseInt(data.substr(2+64,64), 16) + ""
+    let guess = parseInt(data.substr(2+64*2,64), 16) + ""
+    let gameindex = parseInt(data.substr(2+64*3,64), 16) + ""
+    let lotterynumber = parseInt(data.substr(2+64*4,64), 16) + ""
+    let timestamp = parseInt(data.substr(2+64*5,64), 16) + ""
+    let SentPrizeToWinner = {winner:winner,money:money,guess:guess,gameindex:gameindex,lotterynumber:lotterynumber,timestamp:timestamp }
+    return SentPrizeToWinner
+}
 
+function decodeSentDeveloperFee(data) {
+    let amount = parseInt(data.substr(2,64), 16) + ""
+    let balance = parseInt(data.substr(2+64,64), 16) + ""
+    let SentDeveloperFee = {amount:amount,balance:balance};
+    return SentDeveloperFee;
+}
 
 Rx.Observable.fromEvent($('#getBettingStasticsBtn'),'click')        // fromEvent 監聽事件發生
 .mergeMap(()=>{                                                     // mergeMap 承接上個結果 繼續做事 回傳Observable 則使用mergeMap, 如回傳一般值 使用 map 就好
@@ -99,8 +104,8 @@ Rx.Observable.fromEvent($('#getBettingStasticsBtn'),'click')        // fromEvent
     $('#getBettingStasticsResult').html(JSON.stringify(result))     // 模擬合約結果寫入 getBettingStasticsResult
     let ether=parseFloat(document.getElementById('ether').value);   // 抓 ether 值
     return send('getBettingStastics',{value:web3.toWei(ether)})     // 正式送出 getBettingStastics 交易 (跳出 MetaMask 視窗)
-}).retry()                                                          // 當過程中發生錯誤 重新註冊Observable 會從Rx.Observable.fromEvent 開始
-.subscribe({                                                        // 註冊此 Observable(在此之前是不會做任何事)
+}).retry()                                                          // 當過程中發生錯誤 重新訂閱Observable 會從Rx.Observable.fromEvent 開始
+.subscribe({                                                        // 訂閱此 Observable(在此之前是不會做任何事)
     next:({result})=>{                                              // 符合以上流程得到該次result並處理
         $('#getBettingStasticsTxid').html(result)                   // 寫入Txid
     },error:(error)=>{                                              // 反之得到error 並處理
@@ -132,7 +137,7 @@ Rx.Observable.fromEvent($('#findWinnersBtn'),'click')
 })
 
 //===
-// 不影響狀態,交易花 gas ,純翠取結果, 故 不如只做call 取返回值就好
+// 不影響狀態,交易花 gas ,純粹取結果, 故 不如只做call 取返回值就好
 //===
 // .mergeMap((result)=>{
 //     $('#findWinnersResult').html(JSON.stringify(result))                    // 模擬合約結果寫入 findWinnersResult
@@ -147,9 +152,31 @@ Rx.Observable.fromEvent($('#findWinnersBtn'),'click')
 Rx.Observable.fromEvent($('#finishBtn'),'click')
 .mergeMap(()=>{
     return send('finish')                                                       // 送出 finish 交易
-}).retry().subscribe({
-    next:(txid)=>{
-        $('#finishTxid').html(txid)                                             // 寫入Txid
+}).retry().mergeMap((txid)=>{
+    console.log(txid)
+    $('#finishTxid').html('')
+    return Rx.Observable.interval(1000).mergeMap((d)=>{
+        $('#finishTxid').append('.')                                            // result:...(效果
+        return getReceipt(txid)
+    }).filter(data=>data!=null)                                                 // 過濾掉結果為 null 的值
+    .first()                                                                    // 只取第一個 不然 interval 會無止境 觸發下去
+}).map(data=>{
+    JSON.parse(JSON.stringify(data))
+    let result1=''
+    let result2=''
+    for(let log of data.logs){
+        if(log.topics[0]==="0x16772b6ac3e9823e1f39326dbe356dac767fad821f4a2af003066838235e1bbd"){
+            let SentPrizeToWinner=decodeSentPrizeToWinner(log.data)
+            result1 += `,玩家猜測值：${SentPrizeToWinner.guess},答案：${SentPrizeToWinner.lotterynumber},金額：${SentPrizeToWinner.money},時間戳：${SentPrizeToWinner.timestamp},贏家：${SentPrizeToWinner.winner}\n`;
+        }else if(log.topics[0]==="0xf758ff59202247fe26bd4bd951f620cf543dc36b500de667d055cb5816def873"){
+            let SentDeveloperFee = decodeSentDeveloperFee(log.data)
+            result2 = `累計總額：${SentDeveloperFee.amount},餘額：${SentDeveloperFee.balance}`
+        }
+    }
+    return result1 ? result2 + result1  : "沒有贏家"
+}).subscribe({
+    next:(data)=>{
+        $('#finishResult').html(data)                                             // 寫入finishResult
     }
 });
 
@@ -159,7 +186,7 @@ Rx.Observable.fromEvent($('#setBettingConditionBtn'),'click')
     let _bettingprice=parseInt(document.getElementById('_bettingprice').value); // 抓取 _bettingprice
     return send('setBettingCondition',_contenders,_bettingprice)                // 送出 setBettingCondition 交易 (跳出 MetaMask 視窗)
 }).retry().subscribe({
-    next:(txid)=>{
+    next:(result)=>{
         $('#setBettingConditionTxid').html(txid)
     }
 });
@@ -172,5 +199,57 @@ Rx.Observable.fromEvent($('#setStatusPriceBtn'),'click')
     next:(txid)=>{
         $('#setStatusPriceTxid').html(txid)                                     // 寫入Txid
     }
-});
+})
 
+
+let eventHistory = []                               // 因發現獲取的history 可能無序,故累積歷史並排序 
+Rx.Observable.fromEvent($('#history'),'click')
+.first()                                            // 只對第一次點擊有效
+.mergeMap(()=>{
+    return Rx.Observable.create((observer) => {
+        /**
+         * 監聽事件發生
+         */
+        eventSentDeveloperFee.watch((e,r)=>{
+            if(e){observer.error('error',e);return}
+            result = JSON.parse(JSON.stringify(r)); 
+            observer.next(result);
+        })
+    })
+})
+.distinct(data=>data.transactionHash)               // 以transactionHash為目標,過濾重複元素
+.mergeMap((data,a)=>{
+    return getReceipt(data.transactionHash)
+}).map((data)=>{
+    eventHistory.push(data);                        // 因發現獲取的history 可能無序,故累積歷史並排序 
+    eventHistory = eventHistory.sort((x,y)=>{return x.blockNumber-y.blockNumber});
+    return eventHistory;
+})
+.subscribe({
+    next:(data)=>{
+        // 人工組合成表格
+        console.log("=============")
+        let tableContext = "";
+        let index=0;
+        for(let item of data){
+            let SentPrizeToWinnerRow='<td colspan="6"></td></tr>'
+            let SentDeveloperFeeRow="";
+            
+            for(let log of item.logs){
+                if(log.topics[0]==="0x16772b6ac3e9823e1f39326dbe356dac767fad821f4a2af003066838235e1bbd"){
+                    let SentPrizeToWinner=decodeSentPrizeToWinner(log.data)
+                    if(SentPrizeToWinnerRow === '<td colspan="6"></td></tr>'){ 
+                        SentPrizeToWinnerRow = `<td>${SentPrizeToWinner.guess}</td><td>${SentPrizeToWinner.lotterynumber}</td><td>${SentPrizeToWinner.money}</td><td>${SentPrizeToWinner.timestamp}</td><td>${SentPrizeToWinner.winner}</td></r>`;
+                    }else{
+                        SentPrizeToWinnerRow += `<tr><td>${SentPrizeToWinner.guess}</td><td>${SentPrizeToWinner.lotterynumber}</td><td>${SentPrizeToWinner.money}</td><td>${SentPrizeToWinner.timestamp}</td><td>${SentPrizeToWinner.winner}</td></tr>`
+                    }
+                }else if(log.topics[0]==="0xf758ff59202247fe26bd4bd951f620cf543dc36b500de667d055cb5816def873"){
+                    let SentDeveloperFee = decodeSentDeveloperFee(log.data)
+                    SentDeveloperFeeRow= `<tr><td rowspan="${item.logs.length -1 }">${++index}</td><td rowspan="${item.logs.length -1 }">${SentDeveloperFee.amount}</td><td rowspan="${item.logs.length -1 }">${SentDeveloperFee.balance}</td>`
+                }
+            }
+            tableContext += SentDeveloperFeeRow + SentPrizeToWinnerRow;
+        }
+        $('#event').html(tableContext)
+    }
+})
